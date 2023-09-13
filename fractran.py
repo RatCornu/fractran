9fcae46ff2580424bd142182e0d431eeed417c4f
Samuel Nalin <samuel.nalin@univ-orleans.fr>
2023-09-13 16:37:29 +0200

    bouhouh

--- fractran.py
+++ fractran.py
@@ -5,12 +5,12 @@ def is_divisible(num: int, den: int) -> bool:
 
 def fractran(fractions: List[Tuple[int, int]], n: int) -> int:
     ended: bool = False
-    
+    print(n)
     while not ended:
         ended = True
         for (num, den) in fractions:
             if is_divisible(n, den):
-                n = n * num // den
+                n = (n // den) * num
                 ended = False
                 break
 
--- tests.py
+++ tests.py
@@ -1,8 +1,16 @@
 from fractran import fractran
 
-def test_exemple():
+def test_addition():
      assert fractran([(3, 2)], (2 ** 3) * (3 ** 4)) == 3 ** (3 + 4)
      assert fractran([(3, 2)], (2 ** 5) * (3 ** 2)) == 3 ** (5 + 2)
 
+def test_multiplication():
+     assert fractran([(455, 33), (11, 13), (1, 11), (3, 7), (11, 2), (1, 3)], (2**2) * (3**2) == 5 ** (2*2))
+
 def test_final():
-     assert True
+     fractions = [(17,65), (133,34), (17,19), (23,17), (2233,69), (23,29), (31,23), (74,341), (31,37), (41,31), (129,287), (41,43), (13,41), (1,13), (1,3)]
+     x = 1
+     n = 78*5**x
+     print(fractran(fractions, n))
+
+test_multiplication()
