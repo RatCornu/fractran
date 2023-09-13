a628e68fc35813c80083a76015adc3b453889d1d
pigeonmoelleux <pigeonmoelleux@crans.org>
2023-09-13 16:16:01 +0200

    Ajout GitHub actions

--- tests.py
+++ tests.py
@@ -1,8 +1,8 @@
-from fractran import *
+from fractran import fractran
 
 def test_exemple():
      assert fractran([(3, 2)], (2 ** 3) * (3 ** 4)) == 3 ** (3 + 4)
      assert fractran([(3, 2)], (2 ** 5) * (3 ** 2)) == 3 ** (5 + 2)
 
 def test_final():
-     fractions = []
+     assert True
