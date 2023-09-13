8e9782c0393ddc782600d5e534e33199e593d67b
pigeonmoelleux <pigeonmoelleux@crans.org>
2023-09-13 17:00:47 +0200

    Ajout commentaires

--- fractran.py
+++ fractran.py
@@ -4,6 +4,12 @@ def is_divisible(num: int, den: int) -> bool:
     return num % den == 0
 
 def fractran(fractions: List[Tuple[int, int]], n: int) -> int:
+    """
+    Interpréteur FRACTRAN
+
+    Les fractions sont données avec un numérateur et un dénominateurs entiers positifs sous forme de liste de tuples (numérateur, dénominateur).
+    """
+    
     ended: bool = False
     while not ended:
         ended = True
