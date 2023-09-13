2b1411d24fc8e50579a5edbf5fcdfab1808674c6
pigeonmoelleux <pigeonmoelleux@crans.org>
2023-09-13 16:05:03 +0200

    Fin

--- fractran.py
+++ fractran.py
@@ -1,4 +1,17 @@
 from typing import List, Tuple
 
+def is_divisible(num: int, den: int) -> bool:
+    return num % den == 0
+
 def fractran(fractions: List[Tuple[int, int]], n: int) -> int:
-    return 1
+    ended: bool = False
+    
+    while not ended:
+        ended = True
+        for (num, den) in fractions:
+            if is_divisible(n, den):
+                n = n * num // den
+                ended = False
+                break
+
+    return n
--- tests.py
+++ tests.py
@@ -3,3 +3,6 @@ from fractran import *
 def test_exemple():
      assert fractran([(3, 2)], (2 ** 3) * (3 ** 4)) == 3 ** (3 + 4)
      assert fractran([(3, 2)], (2 ** 5) * (3 ** 2)) == 3 ** (5 + 2)
+
+def test_final():
+     fractions = []
